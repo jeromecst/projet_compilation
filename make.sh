0122644827572811dcc55dbf2acb241a17b2df74
jerome <jexowa@gmail.com>
2020-12-13 23:45:13 +0100

    make clean

--- make.sh
+++ make.sh
@@ -6,9 +6,10 @@ then
 	ocamllex analyseur_lexical.mll 
 	ocaml analyseur_lexical.ml < example.minic
 	menhir -v analyseur_syntaxique.mly
+	ocamlc *.ml *.mli
+
 elif [ "$1" = "clean" ]
 then
 	echo "make clean..."
-	rm *.ml *.mli *.automaton *.conflicts
+	rm *.ml *.mli *.automaton *.conflicts *.cmi *.cmo
 fi
-
